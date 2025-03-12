5cdde1e8d7929307d479db78ea8669657f8a3ca3
lhy1019 <devin@Devin>
2025-03-12 17:55:03 +0800

    update

--- student_agent.py
+++ student_agent.py
@@ -1,5 +1,6 @@
 import numpy as np
 import pickle
+import random
 
 # Load the pre-trained Q-table
 with open("reinforce_policy.pkl", "rb") as f:
@@ -7,15 +8,13 @@ with open("reinforce_policy.pkl", "rb") as f:
 
 # Record important state information
 agent_information = {
-    "prev_0_direction": (0, 0),
-    "prev_1_direction": (0, 0),
     "pickup": 0,
     "stations": [],
+    "target": None,
+    "unvisitied": set(["R", "G", "Y", "B"])
 }
 
 def reset_agent(obs):
-    agent_information['prev_0_direction'] = (0, 0)
-    agent_information["prev_1_direction"] = (0, 0)
     agent_information['pickup'] = 0
     agent_information["stations"] = [
         (obs[2], obs[3]),
@@ -23,6 +22,9 @@ def reset_agent(obs):
         (obs[6], obs[7]),
         (obs[8], obs[9]),
     ]
+    target = random.choice(["R", "G", "Y", "B"])
+    agent_information["target"] = target
+    agent_information["unvisitied"] = set(["R", "G", "Y", "B"])
     
 
 def softmax(x):
@@ -32,12 +34,18 @@ def softmax(x):
 directions = [(0, 1), (0, -1), (1, 0), (-1, 0)]
 counter_directions = [(0, -1), (0, 1), (-1, 0), (1, 0)]
 
-def get_state(obs, prev_0_direction=(0, 0), prev_1_direction=(0, 0), pickup=0):
-    taxi_row,   taxi_col, \
-    Rrow, Rcol, Grow, Gcol, Yrow, Ycol, Brow, Bcol, \
+def get_state(obs, pickup=False, target=None, arrived=False):
+    # For this REINFORCE version we use a simplified state representation:
+    # For example, we take only the obstacle flags.
+    taxi_row, taxi_col, Rrow, Rcol, Grow, Gcol, Yrow, Ycol, Brow, Bcol, \
     obstacle_north, obstacle_south, obstacle_east, obstacle_west, \
     passenger_look, destination_look = obs  
     
+    target_pos = (Rrow, Rcol) if target == "R" else \
+                 (Grow, Gcol) if target == "G" else \
+                 (Yrow, Ycol) if target == "Y" else \
+                 (Brow, Bcol)
+                 
     dR = abs(taxi_row - Rrow) + abs(taxi_col - Rcol)
     dG = abs(taxi_row - Grow) + abs(taxi_col - Gcol)
     dY = abs(taxi_row - Yrow) + abs(taxi_col - Ycol)
@@ -49,19 +57,27 @@ def get_state(obs, prev_0_direction=(0, 0), prev_1_direction=(0, 0), pickup=0):
         on_passenger = 1
     if destination_look and (min(dR, dG, dY, dB) == 0):
         on_destination = 1
-    return (pickup, obstacle_north, obstacle_south, obstacle_east, obstacle_west, passenger_look, on_passenger, destination_look, on_destination)
-    return (obstacle_north, obstacle_south, obstacle_east, obstacle_west, prev_0_direction, prev_1_direction, passenger_look, on_passenger)
+        
+    dr_target, dc_target = target_pos[0] - taxi_row, target_pos[1] - taxi_col  
+    dT = abs(dr_target) + abs(dc_target) 
+          
+    return (pickup,
+            target, dT,
+            obstacle_north, obstacle_south, obstacle_east, obstacle_west, 
+            passenger_look, on_passenger, 
+            destination_look, on_destination)
 
 
-def get_action(obs):
+def get_action(obs, render=False):
     global agent_information, Q_table
     for i in range(4):
         if (obs[2*i+2], obs[2*i+3]) not in agent_information["stations"]:
             reset_agent(obs)
             # print("Resetting agent")
             break
+    
     # state = get_state(obs, agent_information["prev_0_direction"], agent_information["prev_1_direction"])
-    state = get_state(obs, pickup=agent_information["pickup"])
+    state = get_state(obs, pickup=agent_information["pickup"], target=agent_information["target"])
 
     if state not in Q_table:
         action = np.random.randint(0, 6)
@@ -70,10 +86,18 @@ def get_action(obs):
         logits = Q_table[state]
         probs = softmax(logits)
         action = np.random.choice(np.arange(6), p=probs)
+        if render:
+            print(f"Action: {action}, State: {state}")
+            print()
+            
+    if state[2] == 0:
+        if not state[-1] and agent_information['target'] in agent_information["unvisitied"]:
+            agent_information["unvisitied"].remove(agent_information["target"])
+        if agent_information["unvisitied"]:
+            agent_information["target"] = random.choice(list(agent_information["unvisitied"]))
+        else:
+            agent_information["target"] = random.choice(["R", "G", "Y", "B"])
         
-    if action < 4:
-        agent_information["prev_1_direction"] = agent_information["prev_0_direction"]
-        agent_information["prev_0_direction"] = directions[action]
     if not agent_information["pickup"] and state[-3] and action == 4:
         agent_information['pickup'] = 1
         
