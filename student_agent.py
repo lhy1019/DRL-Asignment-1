d1a3afe87f10ad07a461190e7d88e90e6585d15a
lhy1019 <devin@Devin>
2025-03-14 16:05:15 +0800

    q table agent

--- student_agent.py
+++ student_agent.py
@@ -3,15 +3,19 @@ import pickle
 import random
 
 # Load the pre-trained Q-table
-with open("reinforce_policy.pkl", "rb") as f:
+with open("q_table.pkl", "rb") as f:
     Q_table = pickle.load(f)
-
+directions = [(1, 0), (-1, 0), (0, 1), (0, -1), (0, 0)]
+directions_counter = [(-1, 0), (1, 0), (0, -1), (0, 1), (0, 0)]
 # Record important state information
 agent_information = {
     "pickup": 0,
     "stations": [],
-    "target": None,
-    "unvisitied": set(["R", "G", "Y", "B"])
+    "visited": set(),
+    "visit_count": 0,
+    "paseenger_pos": None,
+    "prev_taxi_pos": None,
+    "prev_direction": None
 }
 
 def reset_agent(obs):
@@ -22,51 +26,79 @@ def reset_agent(obs):
         (obs[6], obs[7]),
         (obs[8], obs[9]),
     ]
-    target = random.choice(["R", "G", "Y", "B"])
-    agent_information["target"] = target
-    agent_information["unvisitied"] = set(["R", "G", "Y", "B"])
+
+    visited = set()
+    visited.add((obs[0], obs[1]))
+    agent_information["visited"] = visited
+    agent_information["visit_count"] = 1
+    agent_information["passenger_pos"] = None
+    agent_information["prev_taxi_pos"] = (obs[0], obs[1])
+    agent_information["prev_direction"] = None
     
 
-def softmax(x):
-    ex = np.exp(x - np.max(x))
-    return ex / np.sum(ex)
 
-directions = [(0, 1), (0, -1), (1, 0), (-1, 0)]
-counter_directions = [(0, -1), (0, 1), (-1, 0), (1, 0)]
-
-def get_state(obs, pickup=False, target=None, arrived=False):
+def get_state(obs, visited=0, pickup=False, passenger_pos = None):
     # For this REINFORCE version we use a simplified state representation:
     # For example, we take only the obstacle flags.
     taxi_row, taxi_col, Rrow, Rcol, Grow, Gcol, Yrow, Ycol, Brow, Bcol, \
     obstacle_north, obstacle_south, obstacle_east, obstacle_west, \
     passenger_look, destination_look = obs  
-    
-    target_pos = (Rrow, Rcol) if target == "R" else \
-                 (Grow, Gcol) if target == "G" else \
-                 (Yrow, Ycol) if target == "Y" else \
-                 (Brow, Bcol)
                  
     dR = abs(taxi_row - Rrow) + abs(taxi_col - Rcol)
     dG = abs(taxi_row - Grow) + abs(taxi_col - Gcol)
     dY = abs(taxi_row - Yrow) + abs(taxi_col - Ycol)
     dB = abs(taxi_row - Brow) + abs(taxi_col - Bcol)
-    on_passenger = 0
-    on_destination = 0
     
-    if passenger_look and (min(dR, dG, dY, dB) == 0):
-        on_passenger = 1
-    if destination_look and (min(dR, dG, dY, dB) == 0):
-        on_destination = 1
+    passenger_dir = 5
+    destination_dir = 5
+    
+    if passenger_look:
+        if min(dR, dG, dY, dB) == 0:
+            passenger_dir = 4
+        else:
+            passenger_idx = np.argmin([dR, dG, dY, dB])
+            if passenger_idx == 0:
+                passenger_offset = (Rrow - taxi_row, Rcol - taxi_col)
+            elif passenger_idx == 1:
+                passenger_offset = (Grow - taxi_row, Gcol - taxi_col)
+            elif passenger_idx == 2:
+                passenger_offset = (Yrow - taxi_row, Ycol - taxi_col)
+            elif passenger_idx == 3:
+                passenger_offset = (Brow - taxi_row, Bcol - taxi_col)
+            try:
+                passenger_dir = directions.index(passenger_offset)
+            except:
+                passenger_dir = 5
+                
+                
+    if destination_look:
+        if min(dR, dG, dY, dB) == 0:
+            destination_dir = 4
+        else:
+            destination_idx = np.argmin([dR, dG, dY, dB])
+            if destination_idx == 0:
+                destination_offset = (Rrow - taxi_row, Rcol - taxi_col)
+            elif destination_idx == 1:
+                destination_offset = (Grow - taxi_row, Gcol - taxi_col)
+            elif destination_idx == 2:
+                destination_offset = (Yrow - taxi_row, Ycol - taxi_col)
+            elif destination_idx == 3:
+                destination_offset = (Brow - taxi_row, Bcol - taxi_col)
+            try:
+                destination_dir = directions.index(destination_offset)
+            except:
+                destination_dir = 5
+    
+    if passenger_pos:
+        dP = abs(taxi_row - passenger_pos[0]) + abs(taxi_col - passenger_pos[1])
+        if dP == 0:
+            passenger_dir = 4
         
-    dr_target, dc_target = target_pos[0] - taxi_row, target_pos[1] - taxi_col  
-    dT = abs(dr_target) + abs(dc_target) 
           
-    return (pickup,
-            target, dT,
+    return (pickup, visited,
             obstacle_north, obstacle_south, obstacle_east, obstacle_west, 
-            passenger_look, on_passenger, 
-            destination_look, on_destination)
-
+            passenger_look, passenger_dir, 
+            destination_look, destination_dir)
 
 def get_action(obs, render=False):
     global agent_information, Q_table
@@ -75,30 +107,54 @@ def get_action(obs, render=False):
             reset_agent(obs)
             # print("Resetting agent")
             break
-    
-    # state = get_state(obs, agent_information["prev_0_direction"], agent_information["prev_1_direction"])
-    state = get_state(obs, pickup=agent_information["pickup"], target=agent_information["target"])
+        
+    if agent_information["prev_taxi_pos"]:
+        offset = (obs[0] - agent_information["prev_taxi_pos"][0], obs[1] - agent_information["prev_taxi_pos"][1])
+        if abs(offset[0]) + abs(offset[1]) > 1:
+            reset_agent(obs)
+
+    if (obs[0], obs[1]) not in agent_information["visited"]:
+        agent_information["visited"].add((obs[0], obs[1]))
+        agent_information["visit_count"] += 1
+        
+    if agent_information['pickup']:
+        agent_information['passenger_pos'] = (obs[0], obs[1])
+        
+    state = get_state(obs, pickup=agent_information["pickup"], 
+                        visited=agent_information["visit_count"],
+                        passenger_pos=agent_information["passenger_pos"]
+                      )
 
     if state not in Q_table:
         action = np.random.randint(0, 6)
-        # print("Random action")
+
     else:
-        logits = Q_table[state]
-        probs = softmax(logits)
-        action = np.random.choice(np.arange(6), p=probs)
+        qvals = Q_table[state]
+        sorted_actions = qvals.argsort()  # ascending order
+        best_action = sorted_actions[-1]
+        second_best_action = sorted_actions[-2]
+        third_best_action = sorted_actions[-3]
+        fourth_best_action = sorted_actions[-4]
+        # With probability epsilon_second_best, pick second best action
+        action = best_action
+        prev_action = agent_information["prev_direction"]
+        if prev_action and directions_counter.index(prev_action) == best_action:
+            if np.random.uniform(0, 1) < 0.8:
+                action = second_best_action
+        if np.random.uniform(0, 1) < 0.1:
+            action = np.random.choice([best_action, second_best_action, third_best_action, fourth_best_action])
         if render:
             print(f"Action: {action}, State: {state}")
             print()
-            
-    if state[2] == 0:
-        if not state[-1] and agent_information['target'] in agent_information["unvisitied"]:
-            agent_information["unvisitied"].remove(agent_information["target"])
-        if agent_information["unvisitied"]:
-            agent_information["target"] = random.choice(list(agent_information["unvisitied"]))
-        else:
-            agent_information["target"] = random.choice(["R", "G", "Y", "B"])
+                    
+    passenger_dir = state[7]
+    if not agent_information["pickup"] and passenger_dir == 4 and action == 4:
+        agent_information["pickup"] = 1
+        agent_information["passenger_pos"] = (obs[0], obs[1])
         
-    if not agent_information["pickup"] and state[-3] and action == 4:
-        agent_information['pickup'] = 1
-        
+    if action == 5:
+        agent_information["pickup"] = 0
+    agent_information["prev_taxi_pos"] = (obs[0], obs[1])
+    if action < 4:
+        agent_information["prev_direction"] = directions[action] 
     return action
